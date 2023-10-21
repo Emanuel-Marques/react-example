d1dcb2e616e2175ada85999e1526893f5b05e2df
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2023-10-21 02:33:03 +0100

    Fixando aprendizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/header';
+import LuckyNumbers from './components/LuckyNumbers';
 import Footer from './components/Footer';
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <LuckyNumbers />
       <Footer />
     </>
   );
