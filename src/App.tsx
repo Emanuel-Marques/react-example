1d4228df495daa5d06e278db53778838bd80082b
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2023-10-24 16:40:10 +0100

    Aula sobre prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Header from './components/header';
 import LuckyNumbers from './components/LuckyNumbers';
 import Footer from './components/Footer';
 import './App.css';
+import PastResults from './components/PastResults';
 
 function App() {
   return (
     <>
       <Header />
       <LuckyNumbers />
+      <PastResults />
       <Footer />
     </>
   );
